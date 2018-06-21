cc966ee507c76348dbefe0ca8e8f2ddf4417fe49
mcbilek <mcbil@ASUS-LAPTOP>
2018-06-21 17:14:57 +0300

    query print işlemi logger seviyesine bağlandı.

--- application/hooks/QueryLogHook.php
+++ application/hooks/QueryLogHook.php
@@ -1,35 +1,34 @@
 ﻿<?php
+require_once dirname(__FILE__).'/../config/config.php';
 /* application/hooks/QueryLogHook.php */
-class QueryLogHook {
+class QueryLogHook
+{
 
-    function log_queries() {   
-        $CI =& get_instance();
-        $times = $CI->db->query_times;
-        $dbs    = array();
-        $output = NULL;    
-        $queries = $CI->db->queries;
-        
-        if (count($queries) == 0)
-        {
-            $output .= "no queries\n";
-        }
-        else
-        {
-            foreach ($queries as $key=>$query)
-            {
-                $output .= $query . "\n";
+    function log_queries()
+    {   
+        if ($config['log_threshold']>1) {
+            $CI = & get_instance();
+            $times = $CI->db->query_times;
+            $dbs = array();
+            $output = NULL;
+            $queries = $CI->db->queries;
+            
+            if (count($queries) == 0) {
+                $output .= "no queries\n";
+            } else {
+                foreach ($queries as $key => $query) {
+                    $output .= $query . "\n";
+                }
+                $took = round(doubleval($times[$key]), 3);
+                $output .= "===[took:{$took}]\n\n";
             }
-            $took = round(doubleval($times[$key]), 3);
-            $output .= "===[took:{$took}]\n\n";
+            
+            $CI->load->helper('file');
+            if (! write_file(APPPATH . "/logs/queries.log.txt", $output, 'a+')) {
+                log_message('debug', 'Unable to write query the file');
+            }
         }
-
-        $CI->load->helper('file');
-        if ( ! write_file(APPPATH  . "/logs/queries.log.txt", $output, 'a+'))
-        {
-             log_message('debug','Unable to write query the file');
-        }  
     }
-
 }
 
 ?>
